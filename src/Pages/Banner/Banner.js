12503afa13b4c18c428dd8d3a01ad0a675ab0700
sazidahmednassir <sazidahmednassir@gmail.com>
2022-04-17 02:25:16 -0800

    navabr & banner structure done

---
+++ src/Pages/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 cushea">
+                        <h2 className='cush' >Hi , I am the  Professional Occasion Photographer</h2>
+                        <p>Capturing life’s cherished moments is what we love to do. Weddings, Party & Corporate occasion is my best service. I love working with people and making them look their best. My photos will capture the joy and happiness of your special day!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+
+const Home = () => {
+    return (
+        <>
+            <Banner></Banner>
+        </>
+    );
+};
+
+export default Home;
