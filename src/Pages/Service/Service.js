2398cbbad8e63cd61c65e139f5804f7fbefe34a3
sazidahmednassir <sazidahmednassir@gmail.com>
2022-04-17 08:48:18 -0800

    service added

---
+++ src/Pages/Service/Service.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Service.css';
+
+const Service = ({service}) => {
+    const { name, img, description, price} = service;
+    return (
+        <div className='service mt-3'>
+            <img className='cusimg '   src={img} alt="" />
+            <h2 className='card-title text-dark font-weight-bold' >{name}</h2>
+            <p className=' card-text text-danger  font-weight-bold '>Price: ${price}</p>
+            <p className=' card-text   font-weight-bold'><small>{description}</small></p>
+            <button  className='btn btn-danger  '>Book: {name}</button>
+        </div>
+    );
+};
+
+export default Service;
